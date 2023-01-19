458c53b3c29cba61cec9744c43d6c200c69b6e23
Mykhailo Pasiechnyk <105516817+MykhailoPasiechnyk@users.noreply.github.com>
2023-01-19 17:57:29 +0100

    Create docker-script.sh

---
+++ docker-script.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+REPONAME="pasiechnyk/flask-hello"
+
+LOCAL_DIGEST=$(sudo docker inspect --format='{{index .RepoDigests 0}}' pasiechnyk/flask-hello:latest | sed -r 's/^[^@]+//' | cut -c2-)
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_UNAME}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+HUB_DIGEST=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/${REPONAME}/tags/?page_size=100 | jq -r '.results|.[]|.digest')
+
+
+if [[ "$LOCAL_DIGEST" == "$HUB_DIGEST" ]]; then
+ 
+else
+    sudo docker pull pasiechnyk/flask-hello:latest
+    sudo docker stop flask-hello
+    sudo docker run -d --rm --name flask-hello -p 80:81 pasiechnyk/flask-hello
+fi
